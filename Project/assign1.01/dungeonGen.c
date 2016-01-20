af58b444fc01081428c572afa46dca9f638988c1
Gregory Charles Steenhagen - student <gcs@pyrite-n1.cs.iastate.edu>
2016-01-19 19:44:08 -0600

    fixed the compile error by removing an unused array

--- Project/assign1.01/dungeonGen.c
+++ Project/assign1.01/dungeonGen.c
@@ -3,10 +3,6 @@
 int main(int argc, char *argv[]){
   int i;
   int j;
-  char rooms[10];
-  for(i = 0;i< 10;i++){
-    rooms[i] ='.';
-  }
   for(i = 0;i<21;i++){
     for(j = 0; j< 80;j++){
       printf(".");
