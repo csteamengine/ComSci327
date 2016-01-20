69c4f70d41257902bf7526b13477a9cf2f085da1
csteamengine <gcs@iastate.edu>
2016-01-19 21:54:47 -0600

    messed with dungeonGen and got it to store in an array

--- Project/assign1.01/dungeonGen.c
+++ Project/assign1.01/dungeonGen.c
@@ -3,9 +3,19 @@
 int main(int argc, char *argv[]){
   int i;
   int j;
+  char grid[21][80];
   for(i = 0;i<21;i++){
     for(j = 0; j< 80;j++){
-      printf(".");
+      if(j > 30 && j < 60){
+	grid[i][j] = '.';
+      }else{
+	grid[i][j] = '#';
+      }
+    }
+  }
+  for(i = 0; i<21; i++){
+    for(j = 0;j<80;j++){
+      printf("%c",grid[i][j]);
     }
     printf("\n");
   }
