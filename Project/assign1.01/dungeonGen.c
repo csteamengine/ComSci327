6d491ec1ecaf55c5fe5d8da638d25abd73dd12fa
Gregory Charles Steenhagen - student <gcs@pyrite-n2.cs.iastate.edu>
2016-01-25 15:04:58 -0600

    need to figure out boolean problem

--- Project/assign1.01/dungeonGen.c
+++ Project/assign1.01/dungeonGen.c
@@ -1,35 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
- typedef struct Room{
+
+typedef struct Tile{
+  char symbol;
+  int locked = 0;
+}Tile_t;
+typedef struct Room{
     int x_pos;
     int y_pos;
     int x_size;
     int y_size;
-    char symbol;
+    Tile_t floor[][];
   }Room_t;
-  Room_t grid[21][80];
-  Room_t temp;
+Tile_t grid[21][80];
+int j;
+int l;
+void printDungeon();
+void fillDungeon();
 int main(int argc, char *argv[]){
-  int j;
-  int l;
   int seed;
   if(argc == 1){
-    seed = 0;
-     for(j = 0;j< 21;j++){
-     for(l = 0;l<80;l++){
-       grid[j][l].symbol = '.';
-       printf("%c",grid[j][l].symbol);
-     }
-   }
+   seed = 0;
   }else{
    seed = atoi(argv[1]);
+  }
+  printf("Seed: %d\n",seed);
+  fillDungeon();
+  printDungeon();
+  return 0;
+}
+void printDungeon(){
+  for(j = 0;j< 21;j++){
+    for(l = 0;l<80;l++){
+      printf("%c",grid[j][l].symbol);
+    }
+  }
+  printf("Text Line 1\nText Line 2\nText Line 3\n");
+}
+void fillDungeon(){
    for(j = 0;j< 21;j++){
     for(l = 0;l<80;l++){
       grid[j][l].symbol = '.';
-      printf("%c",grid[j][l].symbol);
     }
-   }
   }
-  printf("%d\n",seed);
-  return 0;
 }
