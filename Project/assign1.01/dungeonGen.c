3923859c2bf2ab5d8dc6feb54ded8d8e643d44ab
csteamengine <gcs@iastate.edu>
2016-01-25 12:00:05 -0600

    added some code using structures

--- Project/assign1.01/dungeonGen.c
+++ Project/assign1.01/dungeonGen.c
@@ -1,26 +1,35 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+ typedef struct Room{
+    int x_pos;
+    int y_pos;
+    int x_size;
+    int y_size;
+    char symbol;
+  }Room_t;
+  Room_t grid[21][80];
+  Room_t temp;
 int main(int argc, char *argv[]){
-  int i;
   int j;
-  char grid[21][80];
-  for(i = 0;i<21;i++){
-    for(j = 0; j< 80;j++){
-      if(j > 30 && j < 60){
-	grid[i][j] = '.';
-      }else{
-	grid[i][j] = '#';
-      }
+  int l;
+  int seed;
+  if(argc == 1){
+    seed = 0;
+     for(j = 0;j< 21;j++){
+     for(l = 0;l<80;l++){
+       grid[j][l].symbol = '.';
+       printf("%c",grid[j][l].symbol);
+     }
+   }
+  }else{
+   seed = atoi(argv[1]);
+   for(j = 0;j< 21;j++){
+    for(l = 0;l<80;l++){
+      grid[j][l].symbol = '.';
+      printf("%c",grid[j][l].symbol);
     }
+   }
   }
-  for(i = 0; i<21; i++){
-    for(j = 0;j<80;j++){
-      printf("%c",grid[i][j]);
-    }
-    printf("\n");
-  }
-  for(i = 0;i< 3;i++){
-    printf("Text line: %d\n",i+1);
-  }
+  printf("%d\n",seed);
   return 0;
 }
