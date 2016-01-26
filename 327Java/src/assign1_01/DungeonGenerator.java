a5e47b5c4c9b428947409758ee9fc94939806e9e
csteamengine <gcs@iastate.edu>
2016-01-25 19:03:49 -0600

    Java -- added java files to git under 327Java with fairly accurate gitignore

--- 327Java/src/assign1_01/DungeonGenerator.java
+++ 327Java/src/assign1_01/DungeonGenerator.java
@@ -2,6 +2,10 @@ package assign1_01;
 
 public class DungeonGenerator {
 	public static void main(String[] args){
-		
+		Dungeon dung = new Dungeon();
+		dung.fillDungeon();
+		dung.addRoom();
+		dung.cutCorridors();
+		System.out.println(dung.toString());
 	}
 }
